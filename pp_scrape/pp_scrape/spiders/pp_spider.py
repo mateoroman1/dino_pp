41ebf375f3ae56efa91e1fc3c9e9251e3d789c2c
mateoroman1 <128441281+mateoroman1@users.noreply.github.com>
2026-06-10 22:29:02 -0500

    updated site list to use env

--- pp_scrape/pp_scrape/spiders/pp_spider.py
+++ pp_scrape/pp_scrape/spiders/pp_spider.py
@@ -2,12 +2,16 @@ from pathlib import Path
 
 import scrapy
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+site_list = os.getenv("PP_PAGES").split(",")
+
 class ProjectileSpider(scrapy.Spider):
     name = "projectiles"
-    start_urls = [
-            "https://www.projectilepoints.net/Points/Merkle.html",
-            "https://www.projectilepoints.net/Points/Osceola.html"
-    ]
+    start_urls = site_list
 
     def parse(self, response):
         raw_urls = response.css("img::attr(src)").getall()
