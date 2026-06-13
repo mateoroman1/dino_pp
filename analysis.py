ceadf15667c97f8ac323054883990e979bd14ec3
mateoroman1 <128441281+mateoroman1@users.noreply.github.com>
2026-06-13 18:46:53 -0500

    added umap 2d viz

--- analysis.py
+++ analysis.py
@@ -1,11 +1,22 @@
 import pandas as pd
 import numpy as np
-import ast
+import umap
 
 import matplotlib.pyplot as plt
 from PIL import Image
+import os
 
 from sklearn.neighbors import NearestNeighbors
+
+TYPE_MAP = {
+    "H": "Hopewell",
+    "Hardin": "Hardin",
+    "Osceola": "Osceola",
+    "O": "Osceola",
+    "M": "Merkle",
+    "S": "Snyder",
+    "Pointphotos": "Unknown",
+}
 
 df = pd.read_csv("embeddings/metadata.csv")
 
@@ -24,6 +35,27 @@ nn = NearestNeighbors(
 nn.fit(mean_embeddings)
 
 query_idx = 2
+
+def extract_type(filename):
+
+    stem = (
+        os.path.splitext(
+            os.path.basename(filename)
+        )[0]
+    )
+
+    token = stem.split("_")[-1]
+
+    return TYPE_MAP.get(
+        token,
+        "Unknown"
+    )
+
+df["type"] = df["filename"].apply(
+    extract_type
+)
+
+print(df["type"].value_counts())
 
 def show_neighbors(indices):
     fig, axes = plt.subplots(
@@ -70,4 +102,85 @@ def inspect_query(query_idx):
 
     show_neighbors(indices[0])
 
-inspect_query(query_idx)
+#inspect_query(query_idx)
+
+reducer = umap.UMAP(
+    n_neighbors=20,
+    min_dist=0.1,
+    metric="cosine",
+    random_state=42
+)
+
+embedding_2d = reducer.fit_transform(
+    mean_embeddings
+)
+
+print(embedding_2d.shape)
+
+plt.figure(figsize=(12,12))
+
+plt.scatter(
+    embedding_2d[:,0],
+    embedding_2d[:,1],
+    color="lightgray",
+    s=10,
+    alpha=0.6,
+    zorder=1
+)
+
+hardin_mask = (
+    df["type"] == "Hardin"
+)
+
+plt.scatter(
+    embedding_2d[hardin_mask,0],
+    embedding_2d[hardin_mask,1],
+    s=15
+)
+
+snyder_mask = (
+    df["type"] == "Snyder"
+)
+
+plt.scatter(
+    embedding_2d[snyder_mask,0],
+    embedding_2d[snyder_mask,1],
+    color="red",
+    s=15
+)
+
+osceola_mask = (
+    df["type"] == "Osceola"
+)
+
+plt.scatter(
+    embedding_2d[osceola_mask,0],
+    embedding_2d[osceola_mask,1],
+    color="orange",
+    s=15
+)
+
+merkle_mask = (
+    df["type"] == "Merkle"
+)
+
+plt.scatter(
+    embedding_2d[merkle_mask,0],
+    embedding_2d[merkle_mask,1],
+    color="green",
+    s=15
+)
+
+hopewell_mask = (
+    df["type"] == "Hopewell"
+)
+
+plt.scatter(
+    embedding_2d[hopewell_mask,0],
+    embedding_2d[hopewell_mask,1],
+    color="purple",
+    s=15
+)
+
+
+plt.show()
